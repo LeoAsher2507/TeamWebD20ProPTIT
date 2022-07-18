af8f183636c124a4ce199ec5d0b5723d3e5ba34e
Sesquid <dinhh569@gmail.com>
2022-07-18 21:03:37 +0700

    JS

---
+++ JS/Hùng/hung.js
@@ -0,0 +1,62 @@
+// alert('Hello World')
+// console.log(1)
+
+// VD
+// function myFunction(param) {
+//     if(typeof(param) === 'function')
+//     param('Hoc lap trinh');
+// }
+// function myCallback(value) {
+//     console.log('Value :', value);
+// }
+
+// myFunction(myCallback);
+
+//bai1
+// for(let i = 2; i <= 6; i += 2){
+//     console.log(i);
+// }
+
+// bai2
+// var n = 100, res = 0;
+// for(let i = 1; i <= n; i++) {
+//     res += i;
+// }
+// console.log(res);
+
+// bai3
+// var res = 0;
+// var a = [1, 2, 3, 6, 2, 9, 0];
+// a.forEach(function(value) {
+//     res += (value % 2 == 0);
+// })
+// console.log(res);
+
+// bai4
+// var a = ['A', 'C', 'A', 'A', 'B', 'D', 'B'];
+// var res = [];
+// a.forEach(function(value) {
+//     if(res.includes(value) == false){
+//         res.push(value);
+//     }
+// })
+// console.log(res);
+
+//bai7
+// function myFunction(string, times) {
+//     var res = [];
+//     for(let i = 1; i <= times; i++){
+//         res.push(string);
+//     }
+//     return res;
+// }
+// console.log(myFunction('ahihi', 3));
+
+//bai8
+// var a = [0, 1, false, 2, undefined, '', 3, null];
+// var res = [];
+// a.forEach(function(value) {
+//     if(value) res.push(value);
+// })
+// console.log(res);
+
